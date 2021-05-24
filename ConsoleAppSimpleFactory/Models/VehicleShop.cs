68e2436706fd5fe5832b35302927c766088cf0f0
frank <f.dersjant@inholland.nl>
2021-05-24 20:06:20 +0200

    final

--- ConsoleAppSimpleFactory/Models/VehicleShop.cs
+++ ConsoleAppSimpleFactory/Models/VehicleShop.cs
@@ -1,9 +1,4 @@
 ﻿using ConsoleAppSimpleFactory.Models.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleAppSimpleFactory.Models
 {
